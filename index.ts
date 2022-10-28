8c87be3d4406de8400cda6dd27f66332d8a2f871
Felix Niemeyer <niemeyer.felix@gmail.com>
2022-10-28 21:27:48 +0200

    removing unnecessary code, parsing function call args

--- index.ts
+++ index.ts
@@ -4,106 +4,34 @@ dotenv.config()
 import * as nearAPI from "near-api-js"
 import { startStream, types } from 'near-lake-framework';
 
-interface EventLogData {
-  standard: string,
-  version: string,
-  event: string,
-  data?: unknown,
-};
-
-interface ParasEventLogData {
-  owner_id: string,
-  token_ids: string[],
-};
-
-interface MintbaseEventLogData {
-  owner_id: string,
-  memo: string,
-}
-
 const CONTACT_ADDR = "dev-1666200445140-19169904957827"
 
 async function handleStreamerMessage(
   streamerMessage: types.StreamerMessage
 ): Promise<void> {
-  //const createdOn = new Date(streamerMessage.block.header.timestamp / 1000000)
   streamerMessage
     .shards.map(s => s.chunk)
     .filter(c => c !== null)
     .flatMap(t => t.receipts)
     .forEach(r => {
       if(r.receiverId == CONTACT_ADDR) {
-        console.log(r.receipt)
+        if("Action" in r.receipt) {
+          const action = r.receipt.Action
+          console.log(action)
+          action.actions.forEach((a: types.Action) => {
+            if(a !== "CreateAccount" && "FunctionCall" in a) {
+              const functionCall = a.FunctionCall
+              console.log("called", 
+                functionCall.methodName, 
+                Buffer.from(functionCall.args, 'base64').toString('utf8'), // TODO: is it really utf8?
+                functionCall.gas, 
+                functionCall.deposit
+              )
+            }
+          })
+        }
       }
     })
-  // const relevantOutcomes = streamerMessage
-  //   .shards
-  //   .flatMap(shard => shard.receiptExecutionOutcomes)
-  //   .map(outcome => ({
-  //     receipt: {
-  //       id: outcome.receipt.receiptId, 
-  //       receiverId: outcome.receipt.receiverId,
-  //     },
-  //     events: outcome.executionOutcome.outcome.logs.map(
-  //       (log: string): EventLogData => {
-  //         const [_, probablyEvent] = log.match(/^EVENT_JSON:(.*)$/) ?? []
-  //         try {
-  //           return JSON.parse(probablyEvent)
-  //         } catch (e) {
-  //           return
-  //         }
-  //       }
-  //     )
-  //     .filter(event => event !== undefined)
-  //   }))
-  //   .filter(relevantOutcome =>
-  //     relevantOutcome.events.some(
-  //       event => event.standard === "nep171" && event.event === "nft_mint"
-  //     )
-  //   )
-
-  // let output = []
-  // for (let relevantOutcome of relevantOutcomes) {
-  //   let marketplace = "Unknown"
-  //   let nfts = []
-  //   if (relevantOutcome.receipt.receiverId.endsWith(".paras.near")) {
-  //     marketplace = "Paras"
-  //     nfts = relevantOutcome.events.flatMap(event => {
-  //       return (event.data as ParasEventLogData[])
-  //         .map(eventData => ({
-  //           owner: eventData.owner_id,
-  //           links: eventData.token_ids.map(
-  //             tokenId => `https://paras.id/token/${relevantOutcome.receipt.receiverId}::${tokenId.split(":")[0]}/${tokenId}`
-  //           )
-  //          })
-  //       )
-  //     })
-  //   } else if (relevantOutcome.receipt.receiverId.match(/\.mintbase\d+\.near$/)) {
-  //     marketplace = "Mintbase"
-  //     nfts = relevantOutcome.events.flatMap(event => {
-  //       return (event.data as MintbaseEventLogData[])
-  //         .map(eventData => {
-  //         const memo = JSON.parse(eventData.memo)
-  //         return {
-  //           owner: eventData.owner_id,
-  //           links: [`https://mintbase.io/thing/${memo["meta_id"]}:${relevantOutcome.receipt.receiverId}`]
-  //         }
-  //       })
-  //     })
-  //   } else {
-  //     nfts = relevantOutcome.events.flatMap(event => event.data)
-  //   }
-  //   output.push({
-  //     receiptId: relevantOutcome.receipt.id,
-  //     marketplace,
-  //     createdOn,
-  //     nfts,
-  //   })
-  // }
-  // if (output.length) {
-  //   console.log(`We caught freshly minted NFTs!`)
-  //   console.dir(output, { depth: 5 })
-  // }
 }
 
 (async () => {
