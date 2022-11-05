08ffd95ae0c249714e888c3e908f89a0a526bbd9
Felix Niemeyer <niemeyer.felix@gmail.com>
2022-11-05 12:43:41 +0100

    changing the first migration while nobody is looking

--- migrations/0000-create-db.sql
+++ migrations/0000-create-db.sql
@@ -1,11 +1,17 @@
 DROP TABLE IF EXISTS resources;
 
 CREATE TABLE resources (
-  id TEXT PRIMARY KEY, 
-  title TEXT, 
+  name VARCHAR(36) PRIMARY KEY,  /* [2] */
+  title VARCHAR(256), 
   description TEXT, 
-  pricing TEXT, 
-  price_per_ms TEXT /* stroing as text, because it's too big for BIGINT */
-)
+  contact_info TEXT, 
+  pricing_model VARCHAR(20), 
+  pricing_price_per_ms REAL, 
+  pricing_price_fixed_base REAL, /* [1] */
+  pricing_refund_buffer INTEGER
+);
 
-
+/* 
+[1] let's use reals in the db such that we can make price calculations in the db and tradeoff accuracy for now - later we can use some db that supports bigints in some way 
+[2] size of uuid with dashes 
+*/
